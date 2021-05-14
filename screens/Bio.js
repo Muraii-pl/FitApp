ea0fdfb54be3556bd96d13f1b01d6fc4e688936a
Nekoshy <neko@intmail.pl>
2021-05-14 13:08:32 +0200

    ..

--- screens/Bio.js
+++ screens/Bio.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 import React, { useContext } from 'react'
-import { View, Text, TextInput} from 'react-native'
+import { View, Text, TextInput, StyleSheet} from 'react-native'
 import {BMRContext} from '../context/BMRcontext'
 
  
@@ -20,40 +19,6 @@ const Bio = () => {
             </View>
         </>
         
-=======
-import React, {useState} from 'react'
-import { View, Text, TextInput, StyleSheet, Picker } from 'react-native'
-
-const Bio = () => {
-    const [number, onChangeNumber] = useState(null);
-    const [number2, onChangeNumber2] = useState(null);
-    const [number3, onChangeNumber3] = useState(null);
-    return (
-        <View>
-            <Text>Bio</Text>
-            <TextInput
-            style={styles.input}
-            onChangeText={onChangeNumber}
-            value={number}
-            placeholder="Age"
-            keyboardType="numeric"
-            />
-            <TextInput
-            style={styles.input}
-            onChangeText={onChangeNumber2}
-            value={number2}
-            placeholder="Height"
-            keyboardType="numeric"
-            />
-            <TextInput
-            style={styles.input}
-            onChangeText={onChangeNumber3}
-            value={number3}
-            placeholder="Weight"
-            keyboardType="numeric"
-            />
-        </View>
->>>>>>> 63b68d75399937eba7b2df62253d8b55dac3eeb5
     )
 }
 
--- screens/Home.js
+++ screens/Home.js
@@ -12,20 +12,6 @@ const Home = props => {
     const {value, setValue}= useContext(BMRContext)
 
     const { navigate } = props.navigation
-<<<<<<< HEAD
-
-=======
-    //const bmr = () => {
-        //const plec;
-        //const waga;
-        //const wzrost;
-        //const wiek;
-        //const aktywnosc;
-        //const pal = aktywnosc == 'Znikoma' ? 1.3 : aktywnosc == 'Bardzo mała' ? 1.5 : aktywnosc == 'Umiarkowana' ? 1.7 : aktywnosc == 'Duza' ? 1.9 : 2.4 
-        //const cpm = plec == 'Male' ? ((9.99 * waga) + (6.25 * wzrost) - (4.92 * wiek) + 5) * pal : ((9.99 * waga) + (6.25 * wzrost) - (4.92 * wiek) - 161) * pal
-        //return cpm;
-    //}
->>>>>>> 63b68d75399937eba7b2df62253d8b55dac3eeb5
     return (
         <>
             <Header nav={navigate}/>
