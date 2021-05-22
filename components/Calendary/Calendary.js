107552006e619a634afc19b5cf142709c3e76b57
Nekoshy <neko@intmail.pl>
2021-05-22 14:03:33 +0200

    fixed calendar display

--- components/Calendary/Calendary.js
+++ components/Calendary/Calendary.js
@@ -10,14 +10,35 @@ const Calendary = () => {
     const days = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"]
     const [weekName, setWeekName] = useState([])
     const [weekNumber,setNumberName] = useState([])
-
+    const month = new Date().getMonth()
+    console.log(month)
+    const numOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    console.log(numOfDays[month])
+    let newIndex = 1;
+    let oldMonth = 1;
+    let newWeek = 0;
 
 useEffect(()=>{
     setWeekName([])
     setNumberName([])
     for (let index = -3; index < 3; index++) {
-        setWeekName(weekName => ([...weekName, days.slice([new Date().getDay()+index])[0]]))
-        setNumberName(weekNumber => ([...weekNumber, new Date().getDate()+index+1]))
+    console.log(index, days[new Date().getDay()+index])
+        if (days.slice([new Date().getDay()+index])[0] != undefined){
+            setWeekName(weekName => ([...weekName, days.slice([new Date().getDay()+index])[0]]))
+        } else {
+            setWeekName(weekName => ([...weekName, days[newWeek++]]))
+    };
+
+        if (new Date().getDate()+index+1 > numOfDays[month])  {
+            setNumberName(weekNumber => ([...weekNumber, newIndex++]))
+        } else if ((new Date().getDate()+index+1 <= 0) ) {
+            if (new Date().getDate()+index+1 == 0){
+            setNumberName(weekNumber => ([...weekNumber, numOfDays[month-1]]))}
+            else {
+                setNumberName(weekNumber => ([...weekNumber, numOfDays[month-1]-1]))
+            }
+        } else {
+        setNumberName(weekNumber => ([...weekNumber, new Date().getDate()+index+1]))}
     }
 },[])
 
