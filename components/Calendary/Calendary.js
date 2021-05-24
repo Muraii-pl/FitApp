3ae0e39d4a6e59ad610356d52d1fb0909fe2560c
Muraii-pl <mateusz.glowania@gmail.com>
2021-05-24 10:59:20 +0200

    Reactor display date

--- components/Calendary/Calendary.js
+++ components/Calendary/Calendary.js
@@ -10,10 +10,10 @@ const Calendary = () => {
     const days = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"]
     const [weekName, setWeekName] = useState([])
     const [weekNumber,setNumberName] = useState([])
-    const month = new Date().getMonth()
-    console.log(month)
+    const GetMonth = new Date().getMonth()
+    const GetDay = new Date().getDay()
+    const GetDate = new Date().getDate()
     const numOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    console.log(numOfDays[month])
     let newIndex = 1;
     let oldMonth = 1;
     let newWeek = 0;
@@ -22,23 +22,22 @@ useEffect(()=>{
     setWeekName([])
     setNumberName([])
     for (let index = -3; index < 3; index++) {
-    console.log(index, days[new Date().getDay()+index])
-        if (days.slice([new Date().getDay()+index])[0] != undefined){
-            setWeekName(weekName => ([...weekName, days.slice([new Date().getDay()+index])[0]]))
+        if (days.slice([GetDay+index])[0] != undefined){
+            setWeekName(weekName => ([...weekName, days.slice([GetDay+index])[0]]))
         } else {
             setWeekName(weekName => ([...weekName, days[newWeek++]]))
     };
 
-        if (new Date().getDate()+index+1 > numOfDays[month])  {
+        if (GetDate+index+1 > numOfDays[GetMonth])  {
             setNumberName(weekNumber => ([...weekNumber, newIndex++]))
-        } else if ((new Date().getDate()+index+1 <= 0) ) {
-            if (new Date().getDate()+index+1 == 0){
-            setNumberName(weekNumber => ([...weekNumber, numOfDays[month-1]]))}
+        } else if ((GetDate+index+1 <= 0) ) {
+            if (GetDate+index+1 == 0){
+            setNumberName(weekNumber => ([...weekNumber, numOfDays[GetMonth-1]]))}
             else {
-                setNumberName(weekNumber => ([...weekNumber, numOfDays[month-1]-1]))
+                setNumberName(weekNumber => ([...weekNumber, numOfDays[GetMonth-1]-1]))
             }
         } else {
-        setNumberName(weekNumber => ([...weekNumber, new Date().getDate()+index+1]))}
+        setNumberName(weekNumber => ([...weekNumber, GetDate+index+1]))}
     }
 },[])
 
